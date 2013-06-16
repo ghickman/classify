fabf7e3bdb366ae99d5b42632f426cdc0a958cb4
George Hickman <george@ghickman.co.uk>
2013-06-16 16:51:39 +0100

    Get indirectly inherited methods
    inspect.classify_class_attrs only grabs the directly inherited versions
    of a method so calling super lots is missed. Fix that.

--- classy/library.py
+++ classy/library.py
@@ -1,6 +1,7 @@
 import __builtin__
 import collections
 import inspect
+import operator
 import os
 import pydoc
 
@@ -14,18 +15,16 @@ def docclass(klass, obj, name=None, mod=None, *ignored):
         'parents': [k.__name__ for k in mro]
     })
 
-    attrs = filter(lambda data: pydoc.visiblename(data[0], obj=obj),
-                   pydoc.classify_class_attrs(obj))
+    def get_attrs(obj):
+        all_attrs = filter(lambda data: pydoc.visiblename(data[0], obj=obj),
+                      pydoc.classify_class_attrs(obj))
+        return filter(lambda data: data[2] == obj, all_attrs)
 
-    while attrs:  # the loop of magic
-        thisclass = mro.popleft() if mro else attrs[0][2]
-        attrs, inherited = pydoc._split_list(attrs, lambda t: t[2] is thisclass)
-
-        if thisclass is __builtin__.object:
-            attrs = inherited
+    for cls in mro:
+        if cls is __builtin__.object:
             continue
 
-        attrs.sort()  # Sort attrs by name.
+        attrs = get_attrs(cls)
 
         ## ATTRIBUTES
         for a in build_attributes(filter(lambda t: t[1] == 'data', attrs), obj):
@@ -38,9 +37,7 @@ def docclass(klass, obj, name=None, mod=None, *ignored):
         for method in build_methods(filter(is_method, attrs), klass['parents']):
             klass['methods'].append(method)
 
-        descriptors = filter(lambda t: t[1] == 'data descriptor', attrs)
-
-        attrs = inherited
+        # descriptors = filter(lambda t: t[1] == 'data descriptor', attrs)
 
     return klass
 
@@ -162,6 +159,11 @@ def dispatch(klass, obj, name=None, *args):
     if isinstance(obj, property): return docproperty(*args)
     return docother(*args)
 
+def sort_structures(klass):
+    for lst in ('attributes', 'methods'):
+        klass[lst] = sorted(klass[lst], key=operator.itemgetter('name'))
+    return klass
+
 def build(thing):
     """Build a dictionary mapping of a class."""
     if 'django' in thing:
@@ -195,4 +197,4 @@ def build(thing):
         # dispatch its available methods instead of its value.
         print('instance')
         obj = type(obj)
-    return dispatch(klass, obj, name)
+    return sort_structures(dispatch(klass, obj, name))
