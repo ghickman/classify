e8e82d4196bb3adf11b710850031ed143055ad5c
George Hickman <george@ghickman.co.uk>
2013-06-16 12:54:59 +0100

    Add some basic commands

--- classy/main.py
+++ classy/main.py
@@ -1,15 +1,41 @@
-import pprint
+import argparse
+import os
+import SimpleHTTPServer
+import SocketServer
 import sys
+import webbrowser
 
 from library import build
 
-def run():
 
-    # try:
-    pprint.pprint(build(sys.argv[1]), width=300)
-    # except ImportError:
-    #     sys.stderr.write('Could not import: {0}\n'.format(sys.argv[1]))
-    #     sys.exit(1)
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('klass', metavar='KLASS')
+parser.add_argument('-s', '--serve', action='store_true', dest='serve')
+parser.add_argument('-p', '--port', action='store', dest='port', type=int, default=8000)
+args = parser.parse_args()
+
+
+def serve(port):
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(('', port), Handler)
+    print 'Serving on port: {0}'.format(port)
+    webbrowser.open_new_tab('http://localhost:{0}/classy.html'.format(port))
+    httpd.serve_forever()
+
+
+def run():
+    try:
+        structure = build(args.klass)
+    except ImportError:
+        sys.stderr.write('Could not import: {0}\n'.format(sys.argv[1]))
+        sys.exit(1)
+
+    with open(os.path.join(os.getcwd(), 'classy.html'), 'w') as f:
+        f.write(str(structure))
+
+    if args.serve:
+        serve(args.port)
+
 
 if __name__ == '__main__':
     run()
