4a81e0c568fdda207a0103b3392354edff4cfac2
George Hickman <george@ghickman.co.uk>
2013-06-18 14:09:14 +0100

    Update setup with description

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import find_packages, setup
 setup(
     name='classify',
     version='0.1',
+    description='Render a classes collated members using MRO',
+    long_description=open('README.rst').read(),
     author='George Hickman',
     author_email='george@ghickman.co.uk',
     license='MIT',
