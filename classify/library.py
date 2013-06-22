1f5bb4e6f6efec3463603ecb574c6d85449d66bf
George Hickman <george@ghickman.co.uk>
2013-06-22 14:06:52 +0100

    Change base structure to track method/attr inheritance
    Turn method and attribute lists into a dict of names that hold all
    versions of the object in question, thus allowing overriding to be
    inferred from the ordering of the list.

--- classify/library.py
+++ classify/library.py
@@ -1,10 +1,51 @@
 import __builtin__
 import collections
 import inspect
-import operator
 import os
 import pydoc
 import sys
+
+
+class DefaultOrderedDict(collections.OrderedDict):
+    def __init__(self, default_factory=None, *a, **kw):
+        if (default_factory is not None and
+            not isinstance(default_factory, collections.Callable)):
+            raise TypeError('first argument must be callable')
+        collections.OrderedDict.__init__(self, *a, **kw)
+        self.default_factory = default_factory
+
+    def __getitem__(self, key):
+        try:
+            return collections.OrderedDict.__getitem__(self, key)
+        except KeyError:
+            return self.__missing__(key)
+
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = value = self.default_factory()
+        return value
+
+    def __reduce__(self):
+        if self.default_factory is None:
+            args = tuple()
+        else:
+            args = self.default_factory,
+        return type(self), args, None, None, self.items()
+
+    def copy(self):
+        return self.__copy__()
+
+    def __copy__(self):
+        return type(self)(self.default_factory, self)
+
+    def __deepcopy__(self, memo):
+        import copy
+        return type(self)(self.default_factory,
+                          copy.deepcopy(self.items()))
+    def __repr__(self):
+        return 'OrderedDefaultDict({0}, {1})'.format(self.default_factory,
+                                        collections.OrderedDict.__repr__(self))
 
 
 def classify(klass, obj, name=None, mod=None, *ignored):
@@ -12,6 +53,7 @@ def classify(klass, obj, name=None, mod=None, *ignored):
         raise Exception
 
     mro = collections.deque(inspect.getmro(obj))
+    mro.reverse()
 
     klass.update({
         'name': obj.__name__,
@@ -31,31 +73,32 @@ def classify(klass, obj, name=None, mod=None, *ignored):
         attrs = get_attrs(cls)
 
         ## ATTRIBUTES
-        for a in build_attributes(filter(lambda t: t[1] == 'data', attrs), obj, klass['parents']):
-            klass['attributes'].append(a)
+        for attribute in build_attributes(filter(lambda t: t[1] == 'data', attrs), obj):
+            name = attribute.pop('name')
+            klass['attributes'][name].append(attribute)
 
         ## METHODS
         is_method = lambda t: (t[1] == 'method' or
                                t[1] == 'class method' or
                                t[1] == 'static method')
-        for method in build_methods(filter(is_method, attrs), klass['parents']):
-            klass['methods'].append(method)
+        for method in build_methods(filter(is_method, attrs)):
+            name = method.pop('name')
+            klass['methods'][name].append(method)
 
         # descriptors = filter(lambda t: t[1] == 'data descriptor', attrs)
 
     return klass
 
-def build_attributes(attributes, obj, parents):
+def build_attributes(attributes, obj):
     """Build the Attribute list for the given object."""
     for attr in attributes:
         yield {
             'name': attr[0],
-            'default': getattr(obj, attr[0]),
-            'defined': attr[2].__name__,
-            'order': parents.index(attr[2].__name__),
+            'object': getattr(obj, attr[0]),
+            'defining_class': attr[2],
         }
 
-def build_methods(methods, parents):
+def build_methods(methods):
     for method in methods:
         func = getattr(method[2], method[0])
         # Get the method arguments
@@ -68,18 +111,12 @@ def build_methods(methods, parents):
         yield {
             'name': method[0],
             'docstring': pydoc.getdoc(method[3]),
-            'order': parents.index(method[2].__name__),
-            'defined': method[2].__name__,
+            'defining_class': method[2],
             'arguments': arguments,
             'code': ''.join(lines),
             'lines': {'start': start_line, 'total': len(lines)},
             'file': inspect.getsourcefile(func)
         }
-
-def sort_structures(klass):
-    for lst in ('attributes', 'methods'):
-        klass[lst] = sorted(klass[lst], key=operator.itemgetter('name'))
-    return klass
 
 def build(thing):
     """Build a dictionary mapping of a class."""
@@ -89,8 +126,8 @@ def build(thing):
     sys.path.insert(0, '')
 
     klass = {
-        'attributes': [],
-        'methods': [],
+        'attributes': DefaultOrderedDict(list),
+        'methods': DefaultOrderedDict(list),
         'properties': [],
         'parents': [],
     }
@@ -100,4 +137,4 @@ def build(thing):
         # If the passed obj is an instance of an old-style class,
         # dispatch its available methods instead of its value.
         obj = obj.__class__
-    return sort_structures(classify(klass, obj, name))
+    return classify(klass, obj, name)
