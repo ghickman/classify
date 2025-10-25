f69f586fe67eb44419fa5087b9bfd74fd395e5f5
George Hickman <george@ghickman.co.uk>
2025-10-25 20:36:27 +0100

    Drop OrderedDefaultDict now that dicts are ordered in >=3.6

--- src/classify/library.py
+++ src/classify/library.py
@@ -6,51 +6,6 @@ import pydoc
 import sys
 
 
-class DefaultOrderedDict(collections.OrderedDict):
-    def __init__(self, default_factory=None, *a, **kw):
-        if default_factory is not None and not isinstance(
-            default_factory,
-            collections.abc.Callable,
-        ):
-            msg = "first argument must be callable"
-            raise TypeError(msg)
-        collections.OrderedDict.__init__(self, *a, **kw)
-        self.default_factory = default_factory
-
-    def __getitem__(self, key):
-        try:
-            return collections.OrderedDict.__getitem__(self, key)
-        except KeyError:
-            return self.__missing__(key)
-
-    def __missing__(self, key):
-        if self.default_factory is None:
-            raise KeyError(key)
-        self[key] = value = self.default_factory()
-        return value
-
-    def __reduce__(self):
-        if self.default_factory is None:
-            args = ()
-        else:
-            args = (self.default_factory,)
-        return type(self), args, None, None, self.items()
-
-    def copy(self):
-        return self.__copy__()
-
-    def __copy__(self):
-        return type(self)(self.default_factory, self)
-
-    def __deepcopy__(self, memo):
-        import copy  # noqa: PLC0415
-
-        return type(self)(self.default_factory, copy.deepcopy(self.items()))
-
-    def __repr__(self):
-        return f"OrderedDefaultDict({self.default_factory}, {collections.OrderedDict.__repr__(self)})"
-
-
 def _is_method(t):
     return t[1] == "method" or t[1] == "class method" or t[1] == "static method"
 
@@ -137,8 +92,8 @@ def build(thing):
     sys.path.insert(0, "")
 
     klass = {
-        "attributes": DefaultOrderedDict(list),
-        "methods": DefaultOrderedDict(list),
+        "attributes": collections.defaultdict(list),
+        "methods": collections.defaultdict(list),
         "properties": [],
         "ancestors": [],
         "parents": [],
