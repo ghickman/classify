d2a828b406eedb04932fc826409269918d210d32
George Hickman <george@ghickman.co.uk>
2026-07-27 09:28:54 +0100

    Remove data descriptor func check
    This wasn't firing on anything in tests so likely was dealt with by
    cleaning the input data.

--- src/classify/renderers/string.py
+++ src/classify/renderers/string.py
@@ -93,9 +93,7 @@ def data_descriptors(data_descriptors: dict[str, list[DataDescriptor]], indent) 
     for definitions in data_descriptors.values():
         for i, descriptor in enumerate(definitions):
             for name in ["getter", "setter", "deleter"]:
-                func = getattr(descriptor, name, None)
-                if func is None:
-                    continue
+                func = getattr(descriptor, name)
 
                 if len(definitions) > 1 and i == 0:
                     content += f"{indent}# Defined on: {func.defining_class.name}\n"
