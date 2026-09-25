6beb35e6659741748fa51a1a1b192fa702503b1e
George Hickman <george@ghickman.co.uk>
2026-09-25 11:48:33 +0100

    Tidy up django_setup docstring

--- src/classify/django.py
+++ src/classify/django.py
@@ -5,10 +5,9 @@ def setup_django(settings_path: str) -> None:
     """
     Bootstrap Django
 
-    When running classify against a Django project, rather than Django itself we
-    Django projects sometimes need
+    When running against code that touches Django we need to do some
+    bootstrapping first.
     """
-    # default = "classify.contrib.django.settings"
     os.environ["DJANGO_SETTINGS_MODULE"] = settings_path
 
     import django  # noqa: PLC0415
